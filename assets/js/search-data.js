361a33ed6533519930f63a688e76e7629461dd22
ashkan-pirmani <55592879+ashkan-pirmani@users.noreply.github.com>
2025-01-05 11:05:10 +0000

    Deploying to gh-pages from @ ashkan-pirmani/ashkan-pirmani.github.io@c01b2192a7dd561f83340453620e60897cc909d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A collection of my cool projects.",
+          description: "A collection of my cool projects (Work In Progress).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
